b62d5c198e9bacbd480a03922b39777ad6cd87ae
Kasra Rasaee <krasaee@gmail.com>
2024-06-21 21:02:04 -0700

    minor updates to support the major refactoring of the processor state route graph

--- processor_question_answer.py
+++ processor_question_answer.py
@@ -41,9 +41,7 @@ class AnthropicQuestionAnswerProcessor(BaseProcessorLM):
 
     def apply_states(self, query_states: [dict]):
         route_message = {
-            "processor_id": self.processor.id,
-            "provider_id": self.provider.id,
-            "state_id": self.output_state.id,
+            "route_id": self.output_processor_state.id,
             "type": "query_state_list",
             "query_state_list": query_states
         }
